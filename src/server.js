a8375db38c40886dfe31d9d29b11212269a2dade
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-22 19:27:26 +0400

    feat: add post request

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
 import pool, { createTable } from "./config/sql.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -12,11 +13,24 @@ async function init() {
   }
 
   function serverStart() {
+    app.use(bodyParser.json());
     app.get("/api/products", async (_, response) => {
       try {
         const resultQuery = await pool.query("SELECT * FROM products");
         const rows = resultQuery.rows;
         return response.status(200).json(rows);
+      } catch (error) {
+        return response.status(401).json(error);
+      }
+    });
+    app.post("/api/products", async (req, res) => {
+      const { title, price } = req.body;
+      try {
+        const resultQuery = await pool.query(
+          "INSERT INTO TABLE products(title, price)"
+        );
+        const row = resultQuery.rows[0];
+        return response.status(200).json(row);
       } catch (error) {
         return response.status(401).json(error);
       }
